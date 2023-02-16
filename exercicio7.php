061fe4fc64eba2662587b18b2248924d75254ff3
matheusbortolottibruno <matheus_bortolotti@yahoo.com>
2023-02-15 22:20:04 -0300

    exercicio7

---
+++ exercicio7.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EXERCICIO 7</title>
+</head>
+
+<body>
+    Que turno você estuda?: <input type="text" name="text"><br>
+    <button type="submit">Testar</button><br><br>
+
+    
+    <form action="" method="get">
+       text: <input type="text" name="text"><br>
+
+ ?>
+</body>
+
+</html>
